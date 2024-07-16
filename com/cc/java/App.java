4ac96e743ea6dd08b3c020f86c98dc79a5be578a
Marco Grochowiak <marco.grochowiak@outlook.com>
2024-07-16 09:29:06 +0200

    03// Custom-Konstruktor

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,19 +4,10 @@ public class App {
     
     public static void main(String[] args) {
 
-        // Instanziierung  --> Erzeugen eines Objektes
-        // System.out.println(new Cat());
-
-        // cat --> Referenzvariable
-        // Cat --> Referenztyp(Klasse)
-        Cat cat = new Cat();
+        // Custom-Konstruktor
+        Cat cat = new Cat("Grizabella", "white", 29);
         output("Blick von aussen: " + cat);
         output("Blick von innen: " + cat.getInstanceVariable());
-
-        // Werte setzen
-        cat.name = "Grizabella";
-        cat.furColor = "white";
-        cat.age = 29;
 
         // Werte ausgeben
         output(cat.name);
@@ -26,14 +17,9 @@ public class App {
 
         System.out.println("----------------------------");
 
-        Cat cat2 = new Cat();
+        Cat cat2 = new Cat("Alonzo", "grey", 35);
         output("Blick von aussen: " + cat2);
         output("Blick von innen: " + cat2.getInstanceVariable());
-
-        // Werte setzen
-        cat2.name = "Alonzo";
-        cat2.furColor = "grey";
-        cat2.age = 35;
 
         // Werte ausgeben
         output(cat2.name);
